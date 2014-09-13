26d475be4080029d54df44954531d6791b3ab973
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-09-13 18:47:39 +0200

    Color mandelbrot from volcano texture

--- src/Mandelbrot.js
+++ src/Mandelbrot.js
@@ -5,6 +5,8 @@
 function Mandelbrot(layer) {
   this.shaderPass = new THREE.ShaderPass(SHADERS.mandelbrot);
   this.shaderPass.uniforms.resolution.value = new THREE.Vector2(16 * GU, 9 * GU);
+  this.shaderPass.uniforms.tExplosion.value = Loader.loadTexture( 'res/textures/explosion.png' );
+  Loader.start(function(){}, function(){});
 }
 
 Mandelbrot.prototype.getEffectComposerPass = function() {
--- src/shaders/mandelbrot/fragment.glsl
+++ src/shaders/mandelbrot/fragment.glsl
@@ -1,5 +1,6 @@
 uniform float time;
 uniform vec2 resolution;
+uniform sampler2D tExplosion;
 
 varying vec2 vUv;
 
@@ -17,17 +18,16 @@ float mandelbrot(vec2 c)
 {
     vec2 z = vec2(0.0, 0.0);
 
-    int howFarWeGot = 0;
+    int depth_reached = 0;
     for (int i=0; i<depth; i++) {
-        if (dot(z, z) < 4.0) {
-            z = complexMult(z, z) + c;
-        } else {
-            howFarWeGot = depth - i;
+        if (dot(z, z) > 4.0) {
+            depth_reached = i;
             break;
-        }   
+        }
+        z = complexMult(z, z) + c;
     }
 
-    return 1.0 - float(howFarWeGot) / float(depth);
+    return 1.0 - float(depth - depth_reached) / float(depth);
 }
 
 
@@ -37,8 +37,9 @@ void main(void)
     uv = vec2(uv.x * 3.5 - 2.5, uv.y * 2.0 - 1.0);
 
     vec2 pos = vec2(0.001643721971153, -0.822467633298876);
+    //vec2 pos = vec2(-1.25, 0.1);
     float zoom = pow(2.0, -time) * 3.5;
     vec2 c = pos + uv * zoom;
 
-    gl_FragColor = mandelbrot(c) * vec4(1.0, 0.8, 0.6, 0.3);
+    gl_FragColor = texture2D(tExplosion, vec2(0.0, mandelbrot(c)));
 }
