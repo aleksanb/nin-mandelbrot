585805da3479249eacd7df2acb0b414e190702db
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2014-10-28 21:05:55 +0100

    Make zoom coordinate uniform.

--- src/Mandelbrot.js
+++ src/Mandelbrot.js
@@ -4,8 +4,12 @@
 
 function Mandelbrot(layer) {
   this.shaderPass = new THREE.ShaderPass(SHADERS.mandelbrot);
-  this.shaderPass.uniforms.resolution.value = new THREE.Vector2(16 * GU, 9 * GU);
+  this.shaderPass.uniforms.resolution.value =
+    new THREE.Vector2(16 * GU, 9 * GU);
+  this.shaderPass.uniforms.zoomCoordinate.value =
+    new THREE.Vector2(0.001643721971153, -0.822467633298876);
   this.shaderPass.uniforms.tExplosion.value = Loader.loadTexture( 'res/textures/explosion.png' );
+
   Loader.start(function(){}, function(){});
 }
 
--- src/shaders/mandelbrot/fragment.glsl
+++ src/shaders/mandelbrot/fragment.glsl
@@ -1,11 +1,11 @@
 uniform float time;
 uniform vec2 resolution;
+uniform vec2 zoomCoordinate;
 uniform sampler2D tExplosion;
 
 varying vec2 vUv;
 
 const int depth = 256;
-float iGlobalTime = 1.0;
 
 vec2 complexMult(vec2 a, vec2 b)
 {
@@ -18,7 +18,7 @@ float mandelbrot(vec2 c)
 {
     vec2 z = vec2(0.0, 0.0);
 
-    int depth_reached = 0;
+    int depth_reached = depth;
     for (int i=0; i<depth; i++) {
         if (dot(z, z) > 4.0) {
             depth_reached = i;
@@ -36,10 +36,8 @@ void main(void)
     vec2 uv = (gl_FragCoord.xy / resolution);
     uv = vec2(uv.x * 3.5 - 2.5, uv.y * 2.0 - 1.0);
 
-    vec2 pos = vec2(0.001643721971153, -0.822467633298876);
-    //vec2 pos = vec2(-1.25, 0.1);
     float zoom = pow(2.0, -time) * 3.5;
-    vec2 c = pos + uv * zoom;
+    vec2 c = zoomCoordinate + uv * zoom;
 
     gl_FragColor = texture2D(tExplosion, vec2(0.0, mandelbrot(c)));
 }
